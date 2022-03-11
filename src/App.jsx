6a915c5ed3a8418314892971f3e8231610d023a6
dactrong <trongndph13723@fpt.edu.vn>
2022-03-11 11:11:49 +0700

    update xóa

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,41 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ShowInfo = (props) => {
+  return <div>Show {props.name}</div>
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+function App() {
+  const [count, setCount] = useState(1);
+  const [color, setColor] = useState("green");
+  const [myStatus, setStatus] = useState(false);
+  const [product, setProducts] = useState([{ id: 1, name: "trọng" }, { id: 2, name: "Kiều" }])
+
+  const removeItem = (id) => {
+    const newProduct = product.filter(item => item.id !== id);
+
+    setProducts(newProduct);
+  }
+
+  return <div>
+
+
+    String:<div style={{ background: color, width: 100, height: 100 }}>Conten</div>
+    <br />
+    <button onClick={() => setColor(color + "red")}> Color</button>
+    Boolean:{myStatus ? "Đã kết hôn" : "Chưa kết hôn"} <br />
+    <hr />
+    Number:{count} <br /> <button onClick={() => setCount(count + 1)}  >  Click  </button>
+
+    <button onClick={() => setStatus(!myStatus)}> Status</button>
+    <hr />
+    {myStatus && <div>
+      Arr:{product.map(item => <div>{item.name} <br />
+
+      <button onClick={() => removeItem(item.id)}>Delete</button></div>)}
+    </div>}
+
+  </div>
 }
 
 export default App
