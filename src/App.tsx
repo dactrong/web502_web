0c56027de639a345a1f76348c1a0ccc65b618b36
dactrong <trongndph13723@fpt.edu.vn>
2022-03-21 14:42:24 +0700

    update moi trường

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,12 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import logo from './logo.svg'
-import ShowInfo from './component/ShowInfo'
-import './App.css'
-import { Product } from './type/product'
-import HomePage from './pages/Homepage';
-import ProductPage from './pages/ProductPage';
-import AdminLayout from './pages/layout/AdminLayout';
-import { NavLink, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard'
-import ProductManager from './pages/ProductManager'
-import WebsiteLayout from './pages/layout/WebsiteLayout'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState<number>(0)
-  const [info, setInfo] = useState<Product>({
-    name: "trong",
-    age: 21
-  })
+  const [count, setCount] = useState(0)
 
-  const [product, setProduct] = useState<Product[]>([])
-  useEffect(() => {
-    const getProduct = async () => {
-      const { data } = await axios.get('http://localhost:3001/api/products');
-      setProduct(data);
-    }
-    getProduct();
-  },[])
   return (
     <div className="App">
-      {count} <button onClick={() => setCount(count + 1)}>button</button>
-
-
-      <ShowInfo person={info} />
-      {product.map(item => <div>{item.name}</div>)}
-
-      <header>
-      <ul>
-            <li><NavLink to="/">Home Page</NavLink></li>
-            <li><NavLink to="/product">Product</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-          </ul>
-      </header>
-      <main>
-       <Routes>
-            <Route path="/" element={<WebsiteLayout />}>
-                   <Route index element = {<HomePage />}/>
-                   <Route path="product" element={<ProductPage />}/>
-            </Route>
-            <Route path="admin" element={<AdminLayout />}>
-                     {/* <Route index element={<Navigate to="/admin/dashboard" />} /> */}
-                     <Route path="dashboard" element={<Dashboard />} />
-                     <Route path="product" element={<ProductManager />} />
-            </Route>
-            <Route path="about" element={<h1>About page</h1>}/>
-       </Routes>
-      </main>
+      
     </div>
-
-
-
-
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
-  <App />
-</BrowserRouter>, 
+  <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
 )
